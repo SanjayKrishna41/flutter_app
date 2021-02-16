c4d8a6c4442319aa726d6c0a153e405556b0c787
sanjay <s.sanjaykrishna41@gmail.com>
2021-02-16 17:42:21 +0530

    basic hello world application

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,26 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(
-      Center(child: Text(
-          "Hello World",
-          textDirection: TextDirection.ltr
-      )
-      )
-  );
+
+    MaterialApp(title: "Basic Flutter App",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Basic Flutter App"),
+      ),
+      body: Material(
+          color: Colors.indigo,
+          child: Center(child: Text(
+            "Hello World",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.white,
+                fontSize: 40.0,
+                letterSpacing: 1,
+                wordSpacing: 1),
+          )
+          )
+      ),
+    ),
+    )
+  ); //run app
 }
+
