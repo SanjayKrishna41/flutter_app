b95f954c752f55cb2588774ea32d703c83dbc216
sanjay <s.sanjaykrishna41@gmail.com>
2021-02-17 13:14:54 +0530

    Flutter Row, Column and Expanded widget

--- lib/app_screens/Home.dart
+++ lib/app_screens/Home.dart
@@ -1,27 +1,64 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //return a container widget (its same as div element)
     return Center(
-        child :Container(
-          alignment: Alignment.center,
-          // width: 200.0,
-          // height: 100.0,
-          margin: EdgeInsets.only(left :10.0,right: 10.0),
-          padding: EdgeInsets.all(5.0),
-          color: Colors.blueAccent,
-          child: Text(
-              "Flight",
-              textDirection: TextDirection.ltr,
-            style: TextStyle(
-              decoration: TextDecoration.none,
-              fontSize: 40.0,
-              color: Colors.deepOrange
-            ),
-          ),
-        )
-    );
+        child: Container(
+            alignment: Alignment.center,
+            // width: 200.0,
+            // height: 100.0,
+            padding: EdgeInsets.only(top:40.0,left:10.0),
+            color: Colors.blueAccent,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                      "Flutter Example",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 20.0,
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "using of row and expanded widget",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 24.0,
+                          color: Colors.white),
+                    )),
+                  ],
+                )
+                ,
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                          "Flutter Example column",
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                              decoration: TextDecoration.none,
+                              fontSize: 20.0,
+                              color: Colors.white),
+                        )),
+                    Expanded(
+                        child: Text(
+                          "using of column and expanded widget",
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                              decoration: TextDecoration.none,
+                              fontSize: 24.0,
+                              color: Colors.white),
+                        )),
+                  ],
+                )
+              ],
+            )));
   }
 }
